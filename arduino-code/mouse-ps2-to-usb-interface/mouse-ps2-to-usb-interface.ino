89ac8da7d472ebb44681edc6f897e69531c1cbaf
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2021-03-25 03:40:01 -0700

    updated sketch to output both Clock and Data analog voltages to serial console

--- arduino-code/mouse-ps2-to-usb-interface/mouse-ps2-to-usb-interface.ino
+++ arduino-code/mouse-ps2-to-usb-interface/mouse-ps2-to-usb-interface.ino
@@ -1,14 +1,24 @@
+uint8_t clk = A0;
+uint8_t data = A5;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  //pin 8 input
+  pinMode( 8, INPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.println(analogRead(A0));
+  Serial.print( analogRead(clk) );
+  Serial.print( " " );
+  Serial.println( analogRead(data) );
+  //Serial.println( digitalRead(8) );
 
   //wait for analog-to-digital converter to stabilize 
   //since last reading
-  delay(2);
+  //delay(2);
 }
