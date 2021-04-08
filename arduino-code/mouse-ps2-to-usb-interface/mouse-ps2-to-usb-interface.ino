a9bb48eaa2417f42a6e993e7ce5acb1e4e463bdf
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2021-04-08 05:14:48 -0700

    read and decoded one byte from the mouse

--- arduino-code/mouse-ps2-to-usb-interface/mouse-ps2-to-usb-interface.ino
+++ arduino-code/mouse-ps2-to-usb-interface/mouse-ps2-to-usb-interface.ino
@@ -6,8 +6,11 @@ uint8_t data_analog = A5;
 uint8_t clk_digital_pin = 3;
 uint8_t data_digital_pin = 2;
 
-volatile int testclock = 0;
-int testdata = 0;
+volatile int testClock = 0;
+volatile int testData = 0;
+
+volatile int bitCounter = 0;
+volatile uint8_t mouseOutput = 0;
 
 void setup() {
   // Set up Serial Console
@@ -19,13 +22,14 @@ void setup() {
   /*send starting code to mouse here*/
   //pull the clock low to let mouse know host is sending instruction
   digitalWrite( clk_digital_pin, LOW );
+  testClock++;
   delayMicroseconds( 100 );
 
   //release the clock and send start bit on data line
   pinMode( clk_digital_pin, INPUT );
   digitalWrite( data_digital_pin, LOW ); //start bit
   
-
+  //send reset command - 0xFF
   //send enable reporting data bits to mouse - 0xFA
   /*if( digitalRead( !clk_digital_pin ) ){ //if the clock has gone low, start message
     // 0xFA
@@ -42,79 +46,46 @@ void setup() {
   } */
 
   /* end of starting code to mouse */
+  //read in device ID - 0xAA
 
   //set data pin (7) as input to read from data pin
   pinMode( data_digital_pin, INPUT);
 
   //setting up clock to cause interrupts
   attachInterrupt( digitalPinToInterrupt( clk_digital_pin), readMouse, FALLING );
-
-  attachInterrupt( digitalPinToInterrupt( data_digital_pin), readData, FALLING );
+  //attachInterrupt( digitalPinToInterrupt( data_digital_pin), readData, FALLING );
 }
 
 void loop() {
-  //print out: analog clock voltage + " " + analog data voltage
-  //Serial.print( analogRead(clk_analog) );
-  //Serial.print( " " );
-  //Serial.println( analogRead(data_analog) );
-  //wait for analog-to-digital converter to stabilize 
-  //since last reading
-  //delay(2);
-
-  //print out: digialt clock voltage + " " + digital data voltage
-  //Serial.print( digitalRead(clk_digital_pin) );
-  //Serial.print( " " );
-  //Serial.println( digitalRead(data_digital_pin) );
-  
-  /* Test */
-
-  //int clockValue = digitalRead(clk_digital_pin);
-  //int dataValue = digitalRead(data_digital_pin);
   /*
-  if(digitalRead(clk_digital_pin) == 0){
-    //Serial.println( "Clock went down" );
-    testclock++;
-  }
-
-  if(digitalRead(data_digital_pin) == 0){
-    //Serial.print( "Data went down" );
-    testdata++;
-  }
-
-  if((testclock == 12) || (testclock == 23) ){
+  if((testClock == 12) || (testClock == 23) ){
     Serial.println( "Clock" );
   }
   */
-  //delay microseconds
-  Serial.println( testdata );
+  //if( bitCounter >= 8 ){
+  //  Serial.println( mouseOutput, HEX );
+  //}
+  //Serial.println( testData );
   delay( 100 );
 }
 
 void readMouse() {
-  testclock++;
+  //testClock++;
   
-  //if((testclock == 12) || (testclock == 30) ){
-  //  Serial.println( testclock );
+  //if((testClock == 12) || (testClock == 30) ){
+  //  Serial.println( testClock );
   //}
+
+  if( (bitCounter < 8) && (testClock > 0) ){
+    mouseOutput = mouseOutput | ( digitalRead( data_digital_pin ) << bitCounter );
+    bitCounter++;
+  }
+  if( (bitCounter > 0) && (bitCounter % 8 == 0) ){
+    Serial.println( mouseOutput, HEX );
+    bitCounter++;
+  }
 }
 
 void readData() {
-  testdata++;
+  testData++;
 }
-
-
-  /* Test */
-  //changing analog Read prescalar from 128 to 64
-//  ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
-  //Ahhhh why doesn't this work??!
-  // uncomment as required
-  
-//  ADCSRA |= bit (ADPS0);                               //   2  
-//  ADCSRA |= bit (ADPS1);                               //   4  
-//  ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8  
-//  ADCSRA |= bit (ADPS2);                               //  16 
-//  ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32 
-//  ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64 
-//  ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128
-
-  /* Test */
